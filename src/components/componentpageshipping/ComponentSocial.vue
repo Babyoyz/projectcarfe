45a149430f9e1d118610df24d65e50cee0ebee79
Babyoyz <44115504+Babyoyz@users.noreply.github.com>
2022-03-10 23:26:22 +0700

    วว

--- src/components/componentpageshipping/ComponentSocial.vue
+++ src/components/componentpageshipping/ComponentSocial.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="div-social">
-            <p>asdsad</p>
-            <p>asdsad</p>
-            <p>asdsad</p>
-          
-        </div>
+    <div class="div-social h1">
+        <i class="fa fa-facebook-official " aria-hidden="true"></i>
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+        <i class="fa fa-telegram" aria-hidden="true"></i>
+    </div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-.div-social{
+.div-social {
     position: absolute;
     right: 1rem;
     top: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 }
 </style>
